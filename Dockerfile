77369a99a389edd04bf66f499cd1a07c8e0211be
Aksel Kvitberg <aksel.kvitberg@outlook.com>
2022-07-13 15:07:17 +0200

    Github actions

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@
 WORKDIR /app
 EXPOSE 80
 EXPOSE 443
+EXPOSE 11111
+EXPOSE 30000
+EXPOSE 8080
 
 FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 WORKDIR /src
--- Program.cs
+++ Program.cs
@@ -14,71 +14,46 @@ Console.WriteLine($"IP: {ip}");
 
 builder.Host.UseOrleans(siloBuilder =>
 {
-    ip = builder.Configuration["WEBSITE_PRIVATE_PORTS"];
+    if (builder.Environment.IsDevelopment())
+    {
+        siloBuilder.UseLocalhostClustering()
+            .AddMemoryGrainStorageAsDefault();
+    }
+    else
+    {
+        var siloPort = 11111;
+        var gatewayPort = 30000;
 
-    Console.WriteLine($"IP in orleans: {ip}");
-    
-    var endpointAddress = IPAddress.Parse(builder.Configuration["WEBSITE_PRIVATE_IP"]);
-    var strPorts = builder.Configuration["WEBSITE_PRIVATE_PORTS"].Split(',');
-    if (strPorts.Length < 2)
-        throw new Exception("Insufficient private ports configured.");
-    var (siloPort, gatewayPort) = (int.Parse(strPorts[0]), int.Parse(strPorts[1]));
-    var connectionString = builder.Configuration["ORLEANS_AZURE_STORAGE_CONNECTION_STRING"];
-    
-    Console.WriteLine($"____ USING endpoints {endpointAddress} {siloPort} {gatewayPort}");
+        // Is running as an App Service?
+        if (builder.Configuration["WEBSITE_PRIVATE_IP"] is string ip &&
+            builder.Configuration["WEBSITE_PRIVATE_PORTS"] is string ports)
+        {
+            var endpointAddress = IPAddress.Parse(ip);
+            var splitPorts = ports.Split(',');
+            if (splitPorts.Length < 2) throw new Exception("Insufficient private ports configured.");
 
-    siloBuilder
-        .ConfigureEndpoints(endpointAddress, siloPort, gatewayPort)
-        .Configure<ClusterOptions>(
-            options =>
-            {
-                options.ClusterId = "SimpleCluster";
-                options.ServiceId = "SimpleService";
-            }).UseAzureStorageClustering(options => options.ConfigureTableServiceClient(connectionString));
-    //siloBuilder.AddAzureTableGrainStorageAsDefault(options => options.ConfigureTableServiceClient(connectionString));
+            siloPort = int.Parse(splitPorts[0]);
+            gatewayPort = int.Parse(splitPorts[1]);
+
+            siloBuilder.ConfigureEndpoints(endpointAddress, siloPort, gatewayPort);
+        }
+        else // Assume Azure Container Apps.
+        {
+            siloBuilder.ConfigureEndpoints(siloPort, gatewayPort);
+        }
+
+        var connectionString = builder.Configuration["ORLEANS_AZURE_STORAGE_CONNECTION_STRING"];
+
+        siloBuilder.Configure<ClusterOptions>(
+                options =>
+                {
+                    options.ClusterId = "SimpleCluster";
+                    options.ServiceId = "SimpleOrleans";
+                })
+            .UseAzureStorageClustering(options => options.ConfigureTableServiceClient(connectionString))
+            .AddAzureTableGrainStorageAsDefault(options => options.ConfigureTableServiceClient(connectionString));
+    }
 });
-
-// Add services to the container.
-//
-// builder.Host.UseOrleans(c =>
-// {
-//     c.UseDashboard();
-//     c.Configure<ClusterOptions>(options =>
-//     {
-//         options.ClusterId = "comicDownloader";
-//         options.ServiceId = "ComicDownloader";
-//     });
-//
-//     Console.WriteLine("____ Configuring Orleans");
-//
-//     // var endpointAddress = IPAddress.Parse(builder.Configuration["WEBSITE_PRIVATE_IP"]);
-//     // var strPorts = builder.Configuration["WEBSITE_PRIVATE_PORTS"].Split(',');
-//     // if (strPorts.Length < 2)
-//     //     throw new Exception("Insufficient private ports configured.");
-//     // var (siloPort, gatewayPort) = (int.Parse(strPorts[0]), int.Parse(strPorts[1]));
-//     // var connectionString = builder.Configuration["ORLEANS_AZURE_STORAGE_CONNECTION_STRING"];
-//     //
-//     // Console.WriteLine($"____ USING endpoints {endpointAddress} {strPorts} {gatewayPort}");
-//
-//
-//     //c.ConfigureEndpoints(endpointAddress, siloPort, gatewayPort);
-//     //c.UseAzureStorageClustering(options => options.ConfigureTableServiceClient(builder.Configuration.GetValue<string>("azurestorage:connectionstring")));
-//     c.UseLocalhostClustering();
-//     c.AddMemoryGrainStorageAsDefault();
-//     c.ConfigureLogging(logging => logging.AddConsole());
-//
-//     c.ConfigureApplicationParts(manager =>
-//         manager.AddApplicationPart(Assembly.GetExecutingAssembly()).WithReferences());
-//
-//     Console.WriteLine($"____ Configured Orleans");
-//
-//     c.AddStartupTask(async (provider, token) =>
-//     {
-//         var helloWorld = provider.GetService<IGrainFactory>().GetGrain<IHelloWorld>(Guid.NewGuid());
-//         var result = await helloWorld.Hello();
-//         Console.WriteLine($"____ Result: {result}");
-//     });
-// });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -98,7 +73,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("", () => ip);
+app.MapGet("", () => "Everything is OK");
 app.MapGet("grain/{id}", (string id, IGrainFactory grainfactory) => grainfactory.GetGrain<IHelloWorld>(id).Hello());
 app.MapGet("grain/{id}/{data}", (string id, string data, IGrainFactory grainfactory) => grainfactory.GetGrain<IHelloWorld>(id).Write(data));
 
